674151f5e60375f096aeed457fd738e2b98397f0
Aayush Kumar <code@aayushk.dev>
2026-02-28 17:20:33 +0530

    rename for deployment

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__, static_folder='static')
 
 @app.route('/')
 def index():
-    return send_from_directory(os.getcwd(), 'pastez.html')
+    return send_from_directory(os.getcwd(), 'index.html')
 
 if __name__ == '__main__':
     app.run()
